4677cf04a9f4fff968aacbab6a3384d84c4f365a
BDogg2003 <75631346+BDogg2003@users.noreply.github.com>
2023-08-08 17:18:11 -0400

    Add files via upload
    Added an option to start the playlist index at a specified number

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from pytube.exceptions import RegexMatchError
 
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 import os
 
+music_folder = "mus/" # on windows, include the last '/' or else the program breaks
+video_folder = "vid/" # on windows, include the last '/' or else the program breaks
 
 class YouTubeVideo:
     def __init__(self, givenURL) -> None:
@@ -17,7 +20,7 @@ class YouTubeVideo:
             yt = YouTube(self.givenURL)
             print(yt.streams.filter(only_audio = 'True').asc().first())
             video = yt.streams.filter(only_audio = 'True').asc().first()
-            video.download("mus/", filename_prefix=prefix)  
+            video.download(music_folder, filename_prefix=prefix)  
         except RegexMatchError:
             print('url not found')
         except Exception as e:
@@ -29,21 +32,20 @@ class YouTubeVideo:
             yt = YouTube(self.givenURL)
             video = yt.streams.filter(progressive = 'True').desc().first()
             print(video)
-            video.download("vids/", filename_prefix=prefix)
+            video.download(video_folder, filename_prefix=prefix)
         except RegexMatchError:
             print('url not found')
         except Exception as e:
             print(e)
             
             
-def want_audio(url: str, mode: int) -> None:
+def want_audio(url: str, mode: int, i:int = 1) -> None:
     
     if(mode == 0):
         audio = YouTubeVideo(url)
         audio.download_audio("")
     else:
         p = pl(url)
-        i = 1
         for vid in p.video_urls:
             audio = YouTubeVideo(vid)
             audio.download_audio("{}. ".format(i))
@@ -53,21 +55,20 @@ def want_audio(url: str, mode: int) -> None:
         
         
     #Rename files to mp3
-    path = "mus/"
+    path = music_folder
     dir_list = os.listdir(path)
 
     for dir in dir_list:
-        my_file = 'mus/{}'.format(dir)
+        my_file = '{0}{1}'.format(music_folder, dir)
         base = os.path.splitext(my_file)[0]
         os.rename(my_file, base + '.mp3')
     
-def want_video(url: str, mode: int) -> None:
+def want_video(url: str, mode: int, i: int = 1) -> None:
     if(mode == 0):
         video = YouTubeVideo(url)
         video.download_video("")
     else:
         p = pl(url)
-        i = 1
         for vid in p.video_urls:
             video = YouTubeVideo(vid)
             video.download_video("{}. ".format(i))
@@ -87,6 +88,18 @@ def GUI() -> None:
     f3 = Frame(req)
     Label(f3, text="|").grid(row = 0, column = 0)
     f3.grid(row = 3, column = 1)
+    
+    # starting index
+    
+    index_frame = Frame(req)
+    
+    Label(index_frame, text = "Starting Index").grid(row = 0, column = 0)
+    
+    ind = Entry(index_frame, width=4, justify=CENTER)
+    ind.insert(0, "1")
+    ind.grid(row = 0, column = 1)
+    
+    index_frame.grid(row = 1, column = 2)
     
     #single vid
     sv = Frame(req, padx = 2, pady = 2)
@@ -118,8 +131,8 @@ def GUI() -> None:
     url2 = Entry(playlist)
     url2.grid(row = 6, column = 4, columnspan=2)
     
-    Mp32 = Button(playlist, text = "Download Mp3" ,command= lambda : want_audio(url2.get(), 1))
-    Mp42 = Button(playlist, text = "Download Mp4" ,command= lambda : want_video(url2.get(), 1))
+    Mp32 = Button(playlist, text = "Download Mp3" ,command= lambda : want_audio(url2.get(), 1, int(ind.get())))
+    Mp42 = Button(playlist, text = "Download Mp4" ,command= lambda : want_video(url2.get(), 1, int(ind.get())))
     
     Mp32.grid(row = 7, column = 4, padx = 2, pady = 2)
     Mp42.grid(row = 7, column = 5, padx = 2, pady = 2)
